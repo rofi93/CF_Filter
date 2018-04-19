2d4f61afa52dba9d3b5e9e0552c6b441e36f5744
rofi93 <khairul.basar93@gmail.com>
2018-04-19 20:31:10 +0600

    updated license and implemented filtering

--- filter/views.py
+++ filter/views.py
@@ -1,5 +1,6 @@
 import requests
 
+from django.db.models import F
 from django.shortcuts import render
 from django.views.generic.base import View
 
@@ -16,6 +17,11 @@ def test(request):
         context = {}
         context['results'] = response['result']
 
+        indexes = ContestInfo.objects.all().values('id', 'index').distinct().order_by(F('index'))
+        print(len(indexes))
+        for index in indexes:
+            print(index)
+
         return render(request, 'test.html', context)
 
 
@@ -25,5 +31,43 @@ class Home(View):
 
     def get(self, request, *args, **kwargs):
         self.context['problems'] = Problem.objects.all().order_by('-contest_info__contest__contest_id',
-                                                                  'contest_info__index')
+                                                                  'contest_info__index')[:100]
+        self.get_context()
+
         return render(request, self.template_name, self.context)
+
+    def post(self, request, *args, **kwargs):
+        division = request.POST.get('division')
+        index = request.POST.get('index')
+        tags = request.POST.getlist('tags')
+
+        divisions = []
+        problems = Problem.objects.all()
+
+        if division:
+            if division == '0':
+                divisions.append(0)
+            else:
+                divisions.append(3)
+            if division == '1' or division == '3':
+                divisions.append(1)
+            if division == '2' or division == '3':
+                divisions.append(2)
+
+            problems = problems.filter(contest_info__contest__kind__division__number__in=divisions)
+
+        if index:
+            problems = problems.filter(contest_info__index__contains=index)
+
+        if tags:
+            problems = problems.filter(tags__id__in=tags)
+
+        self.context['problems'] = problems.order_by('-contest_info__contest__contest_id', 'contest_info__index')
+        self.get_context()
+
+        return render(request, self.template_name, self.context)
+
+    def get_context(self):
+        self.context['divisions'] = Division.objects.all().order_by('number')
+        self.context['indexes'] = ContestInfo.objects.all().values('index').distinct().order_by(F('index'))
+        self.context['tags'] = Tag.objects.all().order_by('name')
