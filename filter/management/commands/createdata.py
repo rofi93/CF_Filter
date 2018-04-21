081da5f7d54be3cd65256a791a2c47f2c77ebde4
rofi93 <khairul.basar93@gmail.com>
2018-04-21 13:39:18 +0600

    custom command fixed

--- filter/management/commands/createdata.py
+++ filter/management/commands/createdata.py
@@ -5,12 +5,12 @@ from filter.models import Division, Kind
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        div_0 = Division.objects.get_or_create(name='No Division', number=0)
-        div_1 = Division.objects.get_or_create(name='Division 1', number=1)
-        div_2 = Division.objects.get_or_create(name='Division 2', number=2)
-        div_3 = Division.objects.get_or_create(name='Division 1 + 2', number=3)
+        div_0, created = Division.objects.get_or_create(name='No Division', number=0)
+        div_1, created = Division.objects.get_or_create(name='Division 1', number=1)
+        div_2, created = Division.objects.get_or_create(name='Division 2', number=2)
+        div_3, created = Division.objects.get_or_create(name='Division 1 + 2', number=3)
 
-        Kind.objects.get_or_create(name='Codeforces Regular Round', division=div_1)
-        Kind.objects.get_or_create(name='Codeforces Regular Round', division=div_2)
-        Kind.objects.get_or_create(name='Codeforces Regular Round', division=div_3)
-        Kind.objects.get_or_create(name='Other', division=div_0)
+        kind_1, created = Kind.objects.get_or_create(name='Codeforces Regular Round', division=div_1)
+        kind_2, created = Kind.objects.get_or_create(name='Codeforces Regular Round', division=div_2)
+        kind_3, created = Kind.objects.get_or_create(name='Codeforces Regular Round', division=div_3)
+        kind_4, created = Kind.objects.get_or_create(name='Other', division=div_0)
